89e993684ca4f0eba81a1e4a58e60559a0d7aea2
Rob Brown <brown4homes@yahoo.com>
2020-09-15 15:27:04 -0700

    Added functionality to tetrominoes allowing for random shapes and intervals for moving down

--- app.js
+++ app.js
@@ -43,7 +43,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
     let currentPosition = 4
-    let current = theTetrominoes[0][0]
+    let currentRotation = 0
+
+    let random = Math.floor(Math.random() * theTetrominoes.length)
+    let current = theTetrominoes[random][currentRotation]
 
 
     function draw() {
@@ -52,6 +55,32 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
-    draw()
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
+
+    timerId = setInterval(moveDown, 1000)
+
+    function moveDown() {
+        undraw()
+        currentPosition += width
+        draw()
+        freeze()
+    }
+
+    function freeze() {
+        if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+
+            random = Math.floor(Math.random() * theTetrominoes.length)
+            current = theTetrominoes[random][currentRotation]
+            currentPosition = 4
+            draw()
+        }
+    }
+
+
 
 })
